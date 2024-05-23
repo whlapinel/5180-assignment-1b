399657f7455946373ff332436db4d496a9554f71
whlapinel <140720302+whlapinel@users.noreply.github.com>
2024-05-23 14:01:16 -0400

    complete project

--- app/src/main/java/com/example/assignment1b/MainActivity.java
+++ app/src/main/java/com/example/assignment1b/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             inputTemperature.setText("");
             convertedTemperature.setText("");
             RadioGroup radioGroup = findViewById(R.id.radioGroup);
-            radioGroup.clearCheck();
+            radioGroup.check(R.id.radioButton);
         });
     }
 
